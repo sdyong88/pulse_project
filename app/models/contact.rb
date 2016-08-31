861617fd88e4cb4d22638edf45b52fb8f1f366f0
sdyong88 <sdyong88@gmail.com>
2016-08-31 16:32:22 -0700

    added verfication for Contact emergecy number

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ActiveRecord::Base
   belongs_to :user
   validates :name, presence: true
-  validates_length_of :phone_number, :minimum =>10, :maximum => 10
+  validates_length_of :phone_number, :minimum =>10, :maximum => 12
 
 end
